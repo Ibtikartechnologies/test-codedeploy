a45c21e929c65b6c74d05d92780bb1e452cc7f66
Ibtikartechnologies <operations.enterprise@ibtikar.net.sa>
2020-01-30 11:57:40 +0200

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-sudo echo ${ID} >> /var/www/html/index.html
-sudo echo "This is after install notification" >> /var/www/html/index.html
+#sudo echo ${ID} >> /var/www/html/index.html
+#sudo echo "This is after install notification" >> /var/www/html/index.html
+echo "<!doctype html>
+<html>
+  <head>
+    <title>Testing AWS CodeDeply!</title>
+  </head>
+  <body>
+    <p>We Are testing AWS Code deploy Automatin update And you are on machine ${ID} </p>
+  </body>
+</html>" >> /var/www/html/index.html
